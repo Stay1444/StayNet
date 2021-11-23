f7d8fa51228f1e392fff1601af3b60f78ce79bcf
Stay1444 <staylluc@gmail.com>
2021-11-23 20:07:16 +0100

    added

---
+++ StayNet/Common/Entities/MethodInvokeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace StayNet.Common.Entities
+{
+    internal class MethodInvokeManager
+    {
+        
+        private MethodInvokeManager(){}
+
+        private CancellationToken CToken;
+        private String MethodName;
+        private Object[] Parameters;
+        
+        public static MethodInvokeManager Create(CancellationToken ct, string messageId, object[] invArgs)
+        {
+            MethodInvokeManager manager = new MethodInvokeManager();
+            manager.CToken = ct;
+            manager.MethodName = messageId;
+            manager.Parameters = invArgs;
+            return manager;
+        }
+
+        public bool  TryInvoke()
+        {
+            
+            return false;
+        }
+        
+    }
+}
